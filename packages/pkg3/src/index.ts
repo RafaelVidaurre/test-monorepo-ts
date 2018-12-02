245f002bd2a7d7b20f3d8d1cd949524dd02ae4f1
RafaelVidaurre <narzerus@gmail.com>
2018-12-02 19:06:29 -0300

    done

--- packages/pkg3/src/index.ts
+++ packages/pkg3/src/index.ts
@@ -1,8 +1,10 @@
 import * as p1 from "@ryancavanaugh/pkg1";
 import * as p2 from "@ryancavanaugh/pkg2";
 import { forg } from "@ryancavanaugh/pkg2";
-import { rooto } from "@ryancavanaugh/pkg2";
+import { rootoa } from "@ryancavanaugh/pkg2";
+
+const a = rootoa;
 
 p1.bara();
 p2.fn4();
-console.log(rooto);
+console.log(rootoa);
