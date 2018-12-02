09f989de978444e3fbe5bf855d71f5366c87bfe0
RafaelVidaurre <narzerus@gmail.com>
2018-12-02 17:54:35 -0300

    got build of dec files working, yet no transform of impoorts

--- packages/pkg2/src/index.ts
+++ packages/pkg2/src/index.ts
@@ -1,6 +1,8 @@
 import * as p1 from "@ryancavanaugh/pkg1";
 import { bara } from "@ryancavanaugh/pkg1";
-export { rooto } from '~/foo';
+export { rooto } from "./foo";
+
+export const forg = "444";
 
 export function fn4() {
   p1.bara();
---
+++ packages/pkg3/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const DeclarationBundlerPlugin = require("declaration-bundler-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+      "@ryancavanaugh": path.resolve(__dirname, "node_modules/@ryancavanaugh")
+    },
+    plugins: [
+      new TsconfigPathsPlugin(),
+      new DeclarationBundlerPlugin({
+        moduleName: "pkg1"
+      })
+    ]
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "lib")
+  }
+};
